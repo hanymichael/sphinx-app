88fda3d76ee203353581256951438d9244a636d4
Hany Michael <Hany Michael>
2017-10-14 15:18:00 +0400

    Added a favicon

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ var port = normalizePort(process.env.OPENSHIFT_NODEJS_PORT || '8080');
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.set('port', port);
+app.set('ip', ip);
+
 
 /**
  * Create HTTP server.
